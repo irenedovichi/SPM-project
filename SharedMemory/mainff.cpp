54e79458514c9ec982244592cdb6f4d36c74bdd0
irenedovichi <irenedovichi@gmail.com>
2025-01-15 00:23:43 +0100

    Shared memory

--- SharedMemory/mainff.cpp
+++ SharedMemory/mainff.cpp
@@ -15,7 +15,7 @@ Files are distinguished between "small files" and "big files" depending on BIGFI
 
 
 
-This file was built on top of the primes_a2a.cpp file from the exercises/spmcode7 folder, and ffc_farm.cpp from the ffc folder.
+Note: This file was built on top of the primes_a2a.cpp file from the exercises/spmcode7 folder, and the files in the ffc folder.
 
 
 --------------------
@@ -61,6 +61,7 @@ Decompression:
 
 #include <cstdio>
 #include <iostream>
+# include <string>
 
 #include <cmdline_ff.hpp>
 #include <utility_ff.hpp>
@@ -72,27 +73,240 @@ Decompression:
 using namespace ff;
 
 
+// --------------------------------------------------------------------------------------------------- task --------------------
 struct Task_t {
+    Task_t(unsigned char *ptr, size_t size, const std::string &name):
+        ptr(ptr), size(size), filename(name) {}
 
+    unsigned char *ptr;                 // input pointer
+    size_t size;                        // input size
+    const std::string filename;         // source file name
+    unsigned char *ptrOut = nullptr;    // output pointer
+    size_t cmp_size = 0;                // output size
+    size_t blockid = 1;                 // block identifier (for "BIG files")
+    size_t nblocks = 1;                 // #blocks in which a "BIG file" is split
 };
 
 
+// --------------------------------------------------------------------------------------------------- L-worker ----------------
 struct L_Worker: ff_monode_t<Task_t> {
+    L_Worker(const std::vector<std::string> &files) : files(files) {}
 
+    bool doWorkCompress(const std::string& fname, size_t size) {
+        unsigned char *ptr = nullptr;
+		if (!mapFile(fname.c_str(), size, ptr)) return false;
+		if (size <= BIGFILE_LOW_THRESHOLD) {
+			Task_t *t = new Task_t(ptr, size, fname);
+			ff_send_out(t); // sending to the next stage
+		} else {
+			const size_t fullblocks   = size / BIGFILE_LOW_THRESHOLD;
+			const size_t partialblock = size % BIGFILE_LOW_THRESHOLD;
+			for(size_t i = 0; i < fullblocks; ++i) {
+				Task_t *t = new Task_t(ptr + (i * BIGFILE_LOW_THRESHOLD), BIGFILE_LOW_THRESHOLD, fname);
+				t->blockid = i + 1;
+				t->nblocks = fullblocks + (partialblock > 0);
+				ff_send_out(t); // sending to the next stage
+			}
+			if (partialblock) {
+				Task_t *t = new Task_t(ptr + (fullblocks * BIGFILE_LOW_THRESHOLD), partialblock, fname);
+				t->blockid = fullblocks + 1;
+				t->nblocks = fullblocks + 1;
+				ff_send_out(t); // sending to the next stage
+			}
+		}
+		return true; 
+    }
+
+    bool doWorkDecompress(const std::string& fname, size_t size) { //TODO: adjust, this is a copy-paste from ffc/reader.hpp
+        int r = checkHeader(fname.c_str());
+		if (r < 0) { // fatal error in checking the header
+            if (QUITE_MODE >= 1) 
+                std::fprintf(stderr, "Error: checking header for %s\n", fname.c_str());
+            return false;
+        }
+        if (r > 0) { // it was a small file compressed in the standard way
+            ff_send_out(new Task_t(nullptr, size, fname)); // sending to one Worker
+            return true;
+        }
+        // fname is a tar file (maybe), is was a "BIG file"       	
+        std::string tmpdir;
+        size_t found=fname.find_last_of("/");
+        if (found != std::string::npos)
+            tmpdir = fname.substr(0,found+1);
+        else tmpdir = "./";
+
+        // this dir will be removed in the Writer
+        if (!createTmpDir(tmpdir))  return false;
+        // ---------- TODO: this part should be improved
+        std::string cmd = "tar xf " + fname + " -C" + tmpdir;
+        if ((r = system(cmd.c_str())) != 0) {
+            std::fprintf(stderr, "System command %s failed\n", cmd.c_str());
+            removeDir(tmpdir, true);
+            return false;
+        }
+        // ----------
+        DIR *dir;
+        if ((dir = opendir(tmpdir.c_str())) == NULL) {
+            if (QUITE_MODE >= 1) {
+                perror("opendir");
+                std::fprintf(stderr, "Error: opening temporary dir %s\n", tmpdir.c_str());
+            }
+            removeDir(tmpdir, true);
+            return false;
+        }
+        std::vector<std::string> dirV;
+        dirV.reserve(200); // reserving some space
+        struct dirent *file;
+        bool error=false;
+        while((errno = 0, file = readdir(dir)) != NULL) {
+            std::string filename = tmpdir + "/" + file->d_name;
+            if (!isdot(filename.c_str())) dirV.push_back(filename);
+        }
+        if (errno != 0) {
+            if (QUITE_MODE >= 1) perror("readdir");
+            error = true;
+        }
+        closedir(dir);
+        size_t nfiles = dirV.size();
+        for(size_t i = 0; i < nfiles; ++i) {
+            Task_t *t = new Task_t(nullptr, 0, dirV[i]);
+            t->blockid = i + 1;
+            t->nblocks = nfiles;
+            ff_send_out(t); // sending to the next stage	    
+        }	
+        return !error;
+    }
+
+    Task_t *svc(Task_t *) {
+        // compress or decompress each file assigned to this worker
+        for (const auto &file : files) {
+            struct stat statbuf;
+            if (comp) {
+                // compress the file
+                if (!doWorkCompress(file, statbuf.st_size)) {
+                    std::cerr << "Error compressing file: " << file << std::endl;
+                    return EOS;
+                }
+            } else {
+                // decompress the file
+                if (!doWorkDecompress(file, statbuf.st_size)) {
+                    std::cerr << "Error decompressing file: " << file << std::endl;
+                    return EOS;
+                }
+            }
+        }
+        return EOS;
+    }
+
+    const std::vector<std::string> files;
 };
 
 
+// --------------------------------------------------------------------------------------------------- R-worker ----------------
 struct R_Worker: ff_minode_t<Task_t> {
     R_Worker(const size_t Lw) : Lw(Lw) {}
 
     Task_t *svc(Task_t *in) {
+        bool oneblockfile = (in->nblocks == 1);
+		if (comp) {
+			unsigned char * inPtr  = in->ptr;	
+			size_t          inSize = in->size;
+			
+			// get an estimation of the maximum compression size
+			unsigned long cmp_len = compressBound(inSize);
+
+			// allocate memory to store compressed data in memory
+			unsigned char *ptrOut = new unsigned char[cmp_len];
+			if (compress(ptrOut, &cmp_len, (const unsigned char *)inPtr, inSize) != Z_OK) {
+				if (QUITE_MODE >= 1) std::fprintf(stderr, "Failed to compress file in memory\n");
+				success = false;
+				delete [] ptrOut;
+				delete in;
+				return GO_ON;
+			}
+
+			in->ptrOut   = ptrOut;
+			in->cmp_size = cmp_len;  // now it's the real compression size (see compress in miniz for details)
+
+            if (!oneblockfile) {
+                // Directly pass the task to the Writer
+                ff_send_out(in);
+                return GO_ON;
+            } 
+            // Single block file case: write the compressed data to a file with header = 1
+            std::string outfile{in->filename + SUFFIX};
+            FILE *out_fp = std::fopen(outfile.c_str(), "wb");
+            if (!out_fp) {
+                if (QUITE_MODE >= 1) 
+                    std::fprintf(stderr, "Error opening file %s\n", outfile.c_str());
+                success = false;
+                delete [] in->ptrOut;
+                delete in;
+                return GO_ON;
+            }
+            
+            // Write the header
+            unsigned char header = 1;
+            if (std::fwrite(&header, sizeof(header), 1, out_fp) != 1) {
+                if (QUITE_MODE >= 1) 
+                    std::fprintf(stderr, "Error writing header to file %s\n", outfile.c_str());
+                success = false;
+                std::fclose(out_fp);
+                delete [] in->ptrOut;
+                delete in;
+                return GO_ON;
+            }
+            
+            // Write the compressed data
+            if (std::fwrite(in->ptrOut, 1, in->cmp_size, out_fp) != in->cmp_size) {
+                if (QUITE_MODE >= 1) 
+                    std::fprintf(stderr, "Error writing compressed data to file %s\n", outfile.c_str());
+                success = false;
+            }
+            
+            std::fclose(out_fp);
+            
+            if (REMOVE_ORIGIN) {
+                unlink(in->filename.c_str());
+            }
+            
+            // Clean up and return
+            unmapFile(in->ptr, in->size);	
+            delete [] in->ptrOut;
+            delete in;
+            return GO_ON;
+		}
+		// Decompression part
+
         
+        ////////////////////////////////////////////////////
+		bool remove = !oneblockfile || REMOVE_ORIGIN;
+		if (decompressFile(in->filename.c_str(), in->size, remove) == -1) {
+			if (QUITE_MODE>=1) 
+				std::fprintf(stderr, "Error decompressing file %s\n", in->filename.c_str());
+			success=false;
+		}
+		if (oneblockfile) {
+			delete in;
+			return GO_ON;
+		}
+		return in; // sending to the Writer
+        ////////////////////////////////////////////////////
     }
 
+    void svc_end() {
+		if (!success) {
+			if (QUITE_MODE>=1) std::fprintf(stderr, "R-Worker %ld: Exiting with (some) Error(s)\n", get_my_id());
+			return;
+		}
+    }
+
+    bool success = true;
     const size_t Lw;
 };
 
 
+// --------------------------------------------------------------------------------------------------- writer ------------------
 struct Writer: ff_minode_t<Task_t> {
     Writer(const size_t Rw) : Rw(Rw) {}
 
@@ -104,7 +318,7 @@ struct Writer: ff_minode_t<Task_t> {
 };
 
 
-// ---------------------------------------------------------- main ----------------------------------------------------------
+// --------------------------------------------------------------------------------------------------- main --------------------
 int main(int argc, char *argv[]) {
     if (argc < 2) {
         usage(argv[0]);
@@ -120,15 +334,15 @@ int main(int argc, char *argv[]) {
     // Start the timer //TODO: eventualmente mettere chrono come in quello seq
     ffTime(START_TIME);
 
+    // Distribute the workload among the Lw L-workers
+    std::vector<std::vector<std::string>> partitions = partitionInput(start, argv, argc, Lw);
+
     // Define the FastFlow network ----------------------
     std::vector<ff_node*> LW;
     std::vector<ff_node*> RW;
 
     for (size_t i = 0; i < Lw; ++i) {
-        start = stop;
-        stop = start + size + (more > 0 ? 1 : 0);
-        --more;
-        LW.push_back(new L_Worker(start, stop));
+        LW.push_back(new L_Worker(partitions[i]));
     }
 
     for (size_t i = 0; i < Rw; ++i) {
--- SharedMemory/utility_ff.hpp
+++ SharedMemory/utility_ff.hpp
@@ -46,3 +46,186 @@ static bool isNumber(const char* s, long &n) {
 		return false;
     }
 }
+
+
+// If compdecomp is true (we are compressing), it checks if fname has the suffix SUFFIX,
+// if yes it returns true
+// If compdecomp is false (we are decompressing), it checks if fname has the suffix SUFFIX,
+// if yes it returns false
+static inline bool discardIt(const char *fname, const bool compdecomp) {
+    const int lensuffix=strlen(SUFFIX);
+    const int len      = strlen(fname);
+    if (len>lensuffix &&
+		(strncmp(&fname[len-lensuffix], SUFFIX, lensuffix)==0)) {
+		return compdecomp; // true or false depends on we are compressing or decompressing;
+    }
+    return !compdecomp;
+}
+
+
+// map the file pointed by filepath in memory
+// if size is zero, it looks for file size
+// if everything is ok, it returns the memory pointer ptr
+static inline bool mapFile(const char fname[], size_t &size, unsigned char *&ptr) {
+    // open input file.
+    int fd = open(fname,O_RDONLY);
+    if (fd<0) {
+	if (QUITE_MODE>=1) {
+	    perror("mapFile open");
+	    std::fprintf(stderr, "Failed opening file %s\n", fname);
+	}
+	return false;
+    }
+    if (size==0) {
+	struct stat s;
+	if (fstat (fd, &s)) {
+	    if (QUITE_MODE>=1) {
+		perror("fstat");
+		std::fprintf(stderr, "Failed to stat file %s\n", fname);
+	    }
+	    return false;
+	}
+	size=s.st_size;
+    }
+
+    // map all the file in memory
+    ptr = (unsigned char *) mmap (0, size, PROT_READ, MAP_PRIVATE, fd, 0);
+    if (ptr == MAP_FAILED) {
+	if (QUITE_MODE>=1) {
+	    perror("mmap");
+	    std::fprintf(stderr, "Failed to memory map file %s\n", fname);
+	}
+	return false;
+    }
+    close(fd);
+    return true;
+}
+
+
+// unmap a previously memory-mapped file
+static inline void unmapFile(unsigned char *ptr, size_t size) {
+    if (munmap(ptr, size)<0) {
+	if (QUITE_MODE>=1) {
+	    perror("nummap");
+	    std::fprintf(stderr, "Failed to unmap file\n");
+	}
+    }
+}
+
+
+/*------------------------------------------------------------------------------------------------------------------------------------------------
+Added by me 
+------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+// Function to get files in a directory (recursively or not, depending on RECUR flag)
+std::vector<std::string> getFilesInDir(const std::string &dirPath, bool RECUR) {
+    std::vector<std::string> files;
+    DIR *dir = opendir(dirPath.c_str());
+
+    if (!dir) {
+        std::cerr << "Error opening directory: " << dirPath << std::endl;
+        return files; // Return an empty list on failure
+    }
+
+    struct dirent *entry;
+    while ((entry = readdir(dir)) != nullptr) {
+        std::string fileName = entry->d_name;
+        std::string fullPath = dirPath + "/" + fileName;
+
+        // Skip "." and ".." directories
+        if (fileName == "." || fileName == "..") {
+            continue;
+        }
+
+        struct stat statbuf;
+        if (stat(fullPath.c_str(), &statbuf) == -1) {
+            continue; // Skip if stat fails
+        }
+
+        if (S_ISDIR(statbuf.st_mode)) {
+            // If it's a directory and RECUR is true, recurse into the directory
+            if (RECUR) {
+                std::vector<std::string> subdirFiles = getFilesInDir(fullPath, RECUR);
+                files.insert(files.end(), subdirFiles.begin(), subdirFiles.end());
+            }
+        } else {
+            // It's a file, add it to the list
+            files.push_back(fullPath);
+        }
+    }
+
+    closedir(dir); // Close the directory after processing
+    return files;
+}
+
+
+// Function to process a file without reading it (check suffix, size, and assign to a partition)
+void processFile(const std::string &filePath, std::vector<std::vector<std::string>> &partitions, std::vector<long> &partitionSizes) {
+    struct stat statbuf; // retrieves metadata about a file, including its size, without reading the file's content
+    if (stat(filePath.c_str(), &statbuf) == -1) {
+        return; // Skip if stat fails
+    }
+
+    // Check file size is non-zero
+    if (statbuf.st_size == 0) {
+        if (QUITE_MODE >= 2) {
+            std::fprintf(stderr, "%s has size 0 -- ignored\n", filePath.c_str());
+        }
+        return;
+    }
+
+    // Check file suffix and discard if necessary
+    if (comp && discardIt(filePath.c_str(), true)) {
+        if (QUITE_MODE >= 2) {
+            std::fprintf(stderr, "%s has already a %s suffix -- ignored\n", filePath.c_str(), SUFFIX);
+        }
+        return;
+    }
+
+    if (!comp && discardIt(filePath.c_str(), false)) {
+        if (QUITE_MODE >= 2) {
+            std::fprintf(stderr, "%s does not have a %s suffix -- ignored\n", filePath.c_str(), SUFFIX);
+        }
+        return;
+    }
+
+    // Find the partition with the smallest accumulated size
+    auto min_partition = std::min_element(partitionSizes.begin(), partitionSizes.end());
+    int partitionIndex = std::distance(partitionSizes.begin(), min_partition);
+
+    // Assign the file to that partition
+    partitions[partitionIndex].push_back(filePath);
+
+    // Update the size of the selected partition
+    *min_partition += statbuf.st_size;
+}
+
+
+// Distribute the workload among n workers
+static std::vector<std::vector<std::string>> partitionInput(long start, char *argv[], int argc, int n) {
+	// initialize n partitions
+	std::vector<std::vector<std::string>> partitions(n);
+	// number of files in each partition
+	std::vector<long> partitionSizes(n, 0);
+
+	for(long i = start; i < argc - start; ++i) {		
+		struct stat statbuf; // retrieves metadata about a file, including its size, without reading the file's content
+
+		// Check if it is a directory
+		if (S_ISDIR(statbuf.st_mode)) {
+			// Get all the files in the dir (depending on RECUR, get also the files in the subdirs)
+			std::vector<std::string> files = getFilesInDir(argv[i], RECUR);
+
+			// Process these files
+            for (const std::string &file : files) {
+                processFile(file, partitions, partitionSizes); 
+            }
+
+            continue;
+		} 
+		// argv[i] is a file
+		processFile(argv[i], partitions, partitionSizes);
+		}
+
+	return partitions;
+}
